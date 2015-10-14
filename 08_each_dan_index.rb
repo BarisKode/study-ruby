5bfef44b21a22f1b4b8529fbf7b552168d7c28fd
rohmad-st <rochmad.26@gmail.com>
2015-10-14 20:53:14 +0700

    Perulangan dan index

---
+++ 08_each_dan_index.rb
@@ -0,0 +1,10 @@
+#!/home/rohmat/.rbenv/versions/2.2.3/bin/ruby
+
+@kata = ["s", "a", "y", "a"]
+
+@kata.each_with_index do |item, index|
+  puts "Item : " + item #menggunakan operator tambahan
+  puts "Index: #{index}"
+  puts "----"
+
+end
