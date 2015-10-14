cff4fb89ba0a3c7a1fd49d4fca99651360341c78
rohmad-st <rochmad.26@gmail.com>
2015-10-14 20:57:45 +0700

    Method

---
+++ 09_method.rb
@@ -0,0 +1,8 @@
+#!/home/rohmat/.rbenv/versions/2.2.3/bin/ruby
+
+def sesuatu
+  puts "hasil dari method sesuatu"
+end
+
+#panggil method dengan perintah berikut
+sesuatu
---
+++ 10_method_b.rb
@@ -0,0 +1,8 @@
+#!/home/rohmat/.rbenv/versions/2.2.3/bin/ruby
+
+def provinsi(nama)
+  puts "Nama provinsi: #{nama}"
+end
+
+#panggil method dengan parameter
+provinsi("Sumatera Barat")
