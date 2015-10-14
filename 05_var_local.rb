050add3e951c095467325c7a0ac777f6d13e5a96
rohmad-st <rochmad.26@gmail.com>
2015-10-14 15:29:31 +0700

    Latihan 5
    - variabel local

---
+++ 05_var_local.rb
@@ -0,0 +1,17 @@
+#!/home/rohmat/.rbenv/versions/2.2.3/bin/ruby
+
+class Anggota2
+  def initialize(jumlah)
+    @jumlah_awal = jumlah
+  end
+
+  def tambah_anggota
+    var_local = 1
+    @jumlah_anggota = @jumlah_awal + var_local
+    # puts 'Jumlah anggota baru = ' + @jumlah_anggota.to_s
+    puts @jumlah_anggota
+  end
+end
+
+obj = Anggota2.new(1)
+obj.tambah_anggota
