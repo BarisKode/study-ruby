feaa678255f0e4e7069e504305a4ac4c8630734d
rohmad-st <rochmad.26@gmail.com>
2015-10-14 20:20:26 +0700

    Latihan 6
    - variabel konstan

---
+++ 06_var_constants.rb
@@ -0,0 +1,14 @@
+#!/home/rohmat/.rbenv/versions/2.2.3/bin/ruby
+
+class Konstan
+  VAR1= 'Indonesia'
+  VAR2=34
+
+  def index
+    puts "#{VAR1} mempunyai #{VAR2} provinsi"
+  end
+end
+
+# Membuat obyek
+wew = Konstan.new
+wew.index
